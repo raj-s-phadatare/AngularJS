766f6d4972ebe3872967525eae0bfdfa38694b5c
RajP <rajsphadatare21@gmail.com>
2024-08-13 10:16:07 +0530

    minified JS trial

--- test2/app.js
+++ test2/app.js
@@ -1,22 +1,24 @@
-(function() {
-'use strict';
+// (function() {
+// 'use strict';
 
-angular.module('ControllerAsApp', [])
-.controller('Controller', Controller);
+// angular.module('ControllerAsApp', [])
+// .controller('Controller', Controller);
 
-Controller.$inject = ['$scope'];
-function Controller($scope) {
-    $scope.d1Click = function () {
-        alert("Dummy1 PopUp Successful")
-    };
+// Controller.$inject = ['$scope'];
+// function Controller($scope) {
+//     $scope.d1Click = function () {
+//         alert("Dummy1 PopUp Successful")
+//     };
 
-    $scope.d2Click = function () {
-        alert("Dummy2 PopUp Successful")
-    };
+//     $scope.d2Click = function () {
+//         alert("Dummy2 PopUp Successful")
+//     };
 
-    $scope.d3Click = function () {
-        alert("Dummy3 PopUp Successful")
-    };
-}
+//     $scope.d3Click = function () {
+//         alert("Dummy3 PopUp Successful")
+//     };
+// }
 
-})();
+// })();
+
+!function(){"use strict";function c(c){c.d1Click=function(){alert("Dummy1 PopUp Successful")},c.d2Click=function(){alert("Dummy2 PopUp Successful")},c.d3Click=function(){alert("Dummy3 PopUp Successful")}}angular.module("ControllerAsApp",[]).controller("Controller",c),c.$inject=["$scope"]}();
