a23a160afb5bb1cc2047387babf18b106a767423
RajP <rajsphadatare21@gmail.com>
2024-08-14 16:11:04 +0530

    update

--- test2/app.js
+++ test2/app.js
@@ -27,13 +27,6 @@ function Controller($scope) {
         $scope.dD1 = false;
         $scope.dD3 = $scope.dD3 ? false : true;
     };
-
-    window.onclick = function(){
-        $scope.dD1 = false;
-        $scope.dD2 = false;
-        $scope.dD3 = false;
-    }
-
 }
 
 })();
