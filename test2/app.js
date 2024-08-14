d43d98ae23834d3b7c59c2e620b753579676c74b
RajP <rajsphadatare21@gmail.com>
2024-08-14 15:08:30 +0530

    drop-down trial

--- test2/app.js
+++ test2/app.js
@@ -1,24 +1,37 @@
-// (function() {
-// 'use strict';
+(function() {
+'use strict';
 
-// angular.module('ControllerAsApp', [])
-// .controller('Controller', Controller);
+angular.module('ControllerAsApp', [])
+.controller('Controller', Controller);
 
-// Controller.$inject = ['$scope'];
-// function Controller($scope) {
-//     $scope.d1Click = function () {
-//         alert("Dummy1 PopUp Successful")
-//     };
+Controller.$inject = ['$scope'];
+function Controller($scope) {
+    $scope.d1Click = function () {
+        $scope.dD1.classList.toggle("show");
+    };
 
-//     $scope.d2Click = function () {
-//         alert("Dummy2 PopUp Successful")
-//     };
+    $scope.d2Click = function () {
+        $scope.dD2.classList.toggle("show");
+    };
 
-//     $scope.d3Click = function () {
-//         alert("Dummy3 PopUp Successful")
-//     };
-// }
+    $scope.d3Click = function () {
+        $scope.dD3.classList.toggle("show");
+    };
 
-// })();
+    window.onclick = function (e) {
+        if(!e.target.matches('.dropdown'))
+        {
+            if ($scope.dD1.classList.contains('show')) {
+                $scope.dD1.remove('show');
+            }
+            else if ($scope.dD2.classList.contains('show')) {
+                $scope.dD2.remove('show');
+            }
+            else if ($scope.dD3.classList.contains('show')) {
+                $scope.dD3.remove('show');
+            }
+        }
+    }
+}
 
-!function(){"use strict";function c(c){c.d1Click=function(){alert("Dummy1 PopUp Successful")},c.d2Click=function(){alert("Dummy2 PopUp Successful")},c.d3Click=function(){alert("Dummy3 PopUp Successful")}}angular.module("ControllerAsApp",[]).controller("Controller",c),c.$inject=["$scope"]}();
+})();
