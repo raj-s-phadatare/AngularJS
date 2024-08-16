a92992dc3dc87708fc7057f4f42a3ee389af1c83
RajP <rajsphadatare21@gmail.com>
2024-08-16 10:46:38 +0530

    Updates dropdown

--- test2/app.js
+++ test2/app.js
@@ -27,30 +27,12 @@ function Controller($scope) {
         $scope.dD1 = false;
         $scope.dD3 = $scope.dD3 ? false : true;
     };
+
+    $scope.closeDropdown = function () {
+        $scope.dD1 = false;
+        $scope.dD2 = false;
+        $scope.dD3 = false;
+    }
 }
-
-angular.module('ControllerAsApp', [])
-.directive('closeOnClick', function($document) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            function handleClick(event) {
-                if (!element[0].contains(event.target)) {
-                    scope.$apply(function() {
-                        scope.dD1 = false;
-                        scope.dD2 = false;
-                        scope.dD3 = false;
-                    });
-                }
-            }
-
-            $document.on('click', handleClick);
-
-            scope.$on('$destroy', function() {
-                $document.off('click', handleClick);
-            });
-        }
-    };
-});
 
 })();
