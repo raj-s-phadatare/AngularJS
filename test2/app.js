3d65ec769981486a78a50e5352a426da4452c8cf
RajP <rajsphadatare21@gmail.com>
2024-08-19 11:48:32 +0530

    updates

--- test2/app.js
+++ test2/app.js
@@ -15,21 +15,21 @@ function Controller($scope) {
         $scope.dD2 = false;
         $scope.dD3 = false;
         $scope.dD1 = $scope.dD1 ? false : true;
-        $scope.changeCaret = "caret caret-up";
+        $scope.changeCaret = "caret-up";
     };
 
     $scope.d2Click = function () {
         $scope.dD1 = false;
         $scope.dD3 = false;
         $scope.dD2 = $scope.dD2 ? false : true;
-        $scope.changeCaret = "caret caret-up";
+        $scope.changeCaret = "caret-up";
     };
 
     $scope.d3Click = function () {
         $scope.dD2 = false;
         $scope.dD1 = false;
         $scope.dD3 = $scope.dD3 ? false : true;
-        $scope.changeCaret = "caret caret-up";
+        $scope.changeCaret = "caret-up";
     };
 }
 
