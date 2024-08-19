7040ac8f1a9231a676bc042909336fbe605a3906
RajP <rajsphadatare21@gmail.com>
2024-08-19 14:50:50 +0530

    updates

--- test2/app.js
+++ test2/app.js
@@ -9,27 +9,35 @@ function Controller($scope) {
     $scope.dD1 = false;
     $scope.dD2 = false;
     $scope.dD3 = false;
-    $scope.changeCaret1, $scope.changeCaret2, $scope.changeCaret3 = 'caret';
+    $scope.changeCaret1 = 'caret';
+    $scope.changeCaret2 = 'caret';
+    $scope.changeCaret3 = 'caret';
 
     $scope.d1Click = function () {
-        $scope.dD2, $scope.dD3 = false;
+        $scope.dD2 = false;
+        $scope.dD3 = false;
         $scope.dD1 = $scope.dD1 ? false : true;
-        $scope.changeCaret2, $scope.changeCaret3 = 'caret';
-        $scope.changeCaret1 = "caret-up";
+        $scope.changeCaret2 = 'caret';
+        $scope.changeCaret3 = 'caret';
+        $scope.changeCaret1 = $scope.changeCaret1 ? 'caret' : "caret-up";
     };
 
     $scope.d2Click = function () {
-        $scope.dD1, $scope.dD3 = false;
+        $scope.dD1 = false;
+        $scope.dD3 = false;
         $scope.dD2 = $scope.dD2 ? false : true;
-        $scope.changeCaret1, $scope.changeCaret3 = 'caret';
-        $scope.changeCaret2 = "caret-up";
+        $scope.changeCaret1 = 'caret';
+        $scope.changeCaret3 = 'caret';
+        $scope.changeCaret2 = $scope.changeCaret2 ? 'caret' : "caret-up";
     };
 
     $scope.d3Click = function () {
-        $scope.dD2, $scope.dD1 = false;
+        $scope.dD2 = false;
+        $scope.dD1 = false;
         $scope.dD3 = $scope.dD3 ? false : true;
-        $scope.changeCaret2, $scope.changeCaret1 = 'caret';
-        $scope.changeCaret3 = "caret-up";
+        $scope.changeCaret2 = 'caret';
+        $scope.changeCaret1 = 'caret';
+        $scope.changeCaret3 = $scope.changeCaret3 ? 'caret' : "caret-up";
     };
 }
 
